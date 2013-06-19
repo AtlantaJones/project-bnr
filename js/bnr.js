4d3449b8e6db5bb5b1bbdb76247a7927e91ed356
Andrew Jones <andrew@atlantajones.com>
2013-06-19 00:34:13 -0400

    More progress

--- js/bnr.js
+++ js/bnr.js
@@ -1,11 +1,34 @@
 
 
+// Between the breakpoints of 768 and 992, make sure the slideshow and block beside it
+// are the same height
+function slideHeight() {
+    if(Modernizr.mq('screen and (min-width:768px) and (max-width:1024px)')) {
+        var newHeight = $('#homeSlideShow').css("height");
+        $('#yellowBox').css('height', newHeight);
+    } else {
+        $('#yellowBox').css('height', 'auto');
+    }
+}
+
+window.onorientationchange = function() {
+    slideHeight();
+};
 
 $(document).ready(function() {
+
+    slideHeight();
 
     $('.link-sammich').click(function() {
         $('.nav-drawer').toggleClass('open closed');
         return false;
+    });
+
+
+
+    // Things to do on window resize
+    $(window).resize(function() {
+        slideHeight();
     });
 
 
